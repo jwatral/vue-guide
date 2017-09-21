6b31a48ca00eb57fa927eb8710f8cc8d543c8a11
Jaroslaw Watral <jwatral@users.noreply.github.com>
2017-09-21 15:21:27 +0200

    advanced components finished

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,43 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col-xs-12">
-
-      </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <button @click="selectedComponent = 'appQuote'">Quote</button>
+                <button @click="selectedComponent = 'appAuthor'">Author</button>
+                <button @click="selectedComponent = 'appNew'">New</button>
+                <hr>
+                <keep-alive>
+                    <component :is="selectedComponent">
+                        <p>Default Content</p>
+                    </component>
+                </keep-alive>
+                <!--<app-quote>-->
+                <!--<h2 slot="title">{{ quoteTitle }}</h2>-->
+                <!--<p slot="content">A wonderful Quote</p>-->
+                <!--</app-quote>-->
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
-export default {
+    import Quote from './components/Quote.vue';
+    import Author from './components/Author.vue';
+    import New from './components/New.vue';
 
-}
+    export default {
+        data() {
+            return {
+                quoteTitle: 'The Quote!',
+                selectedComponent: 'appQuote'
+            }
+        },
+        components: {
+            appQuote: Quote,
+            appAuthor: Author,
+            appNew: New
+        }
+    }
 </script>
 
 <style>
