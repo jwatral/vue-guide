03a9de3f2d602ee16df599eecdfadf325758e0c4
Jaroslaw Watral <jwatral@users.noreply.github.com>
2017-09-28 15:30:21 +0200

    finished mixins

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,42 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col-xs-12">
-
-      </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Filters & Mixins</h1>
+                <p>{{ text | toUppercase | to - lowercase }}</p>
+                <hr>
+                <button @click="fruits.push('Berries')">Add new item</button>
+                <input v-model="filterText">
+                <ul>
+                    <li v-for="fruit in filteredFruits">{{ fruit }}</li>
+                </ul>
+                <hr>
+                <app-list></app-list>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
-export default {
+    import List from './List.vue';
+    import {fruitMixin} from './fruitMixin'
 
-}
+    export default {
+        mixins: [fruitMixin],
+        data() {
+            return {
+                text: 'Hello there!',
+            }
+        },
+        filters: {
+            toUppercase(value) {
+                return value.toUpperCase()
+            }
+        },
+        components: {
+            appList: List
+        }
+    }
 </script>
 
 <style>
