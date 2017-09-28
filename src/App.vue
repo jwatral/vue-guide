e7d734862e2edfb888ca09ddcd9e95813e7a8d6c
Jaroslaw Watral <jwatral@users.noreply.github.com>
2017-09-28 15:34:24 +0200

    fixed bootstrap classes in template

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container">
         <div class="row">
-            <div class="col-xs-12">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
 
             </div>
         </div>
