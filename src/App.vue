8813b01f79661b404ee9d5c34c501907c9bfaee3
Jaroslaw Watral <jwatral@users.noreply.github.com>
2017-10-04 20:52:33 +0200

    quiz

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,30 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-
+                <h1 class="text-center">The Super Quiz</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <component :is="mode"></component>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+    import Question from './Question.vue';
+    import Answer from './Answer.vue'
     export default {
+        data() {
+            return {
+                mode: 'app-question'
+            }
+        },
+        components: {
+            appQuestion: Question,
+            appAnswer: Answer
+        }
 
     }
 </script>
