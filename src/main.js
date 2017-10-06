a8cae2cd5b79426b4067a0e905b04cdcd20c9ecd
Jaroslaw Watral <jwatral@users.noreply.github.com>
2017-10-06 08:40:58 +0200

    vue-resource

--- src/main.js
+++ src/main.js
@@ -1,7 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueResource from 'vue-resource';
+
+Vue.use(VueResource);
+Vue.http.options.root = 'https://vuejs-http-c40e2.firebaseio.com/';
+Vue.http.interceptors.push((request, next) => {
+    console.log(request);
+    if(request.method === 'POST') {
+        request.method = 'PUT';
+    }
+    next(response => {
+        response.json = () => { return { messages: response.body }}
+    });
+});
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
-})
+    el: '#app',
+    render: h => h(App)
+});
